38b5f6bdc635b04c9cd38ecd8d631a446dc82700
Ayush Chowdhury <57065963+itsayushch@users.noreply.github.com>
2024-08-26 19:04:48 +0000

    Fix

--- app.py
+++ app.py
@@ -304,4 +304,5 @@ def internal_error(error):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    host = os.environ.get('PORT', '0.0.0.0')
+    app.run(host=host, port=port)
