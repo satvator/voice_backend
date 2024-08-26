9e32212f959e057c26d945d129bdb9579a8b0bd8
Ayush Chowdhury <57065963+itsayushch@users.noreply.github.com>
2024-08-26 19:05:04 +0000

    dynamic host

--- app.py
+++ app.py
@@ -304,5 +304,5 @@ def internal_error(error):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    host = os.environ.get('PORT', '0.0.0.0')
+    host = os.environ.get('HOST', '0.0.0.0')
     app.run(host=host, port=port)
