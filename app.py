a95c6bbee2ebae420f83339c58f2508d1f795bb7
Ayush Chowdhury <57065963+itsayushch@users.noreply.github.com>
2024-08-26 19:14:14 +0000

    remove host

--- app.py
+++ app.py
@@ -304,5 +304,4 @@ def internal_error(error):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    host = os.environ.get('HOST', '0.0.0.0')
-    app.run(host=host, port=port)
+    app.run(port=port)
